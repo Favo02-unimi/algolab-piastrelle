257b764cdb92321af9796c1b0dcf972305453ec0
Luca Favini <luca.favini.02@gmail.com>
2024-06-06 14:21:33 +0200

    fix input generator: `pistrella` coords bounds, `regola` colors

--- input_generator.py
+++ input_generator.py
@@ -3,21 +3,21 @@ import math
 
 def generaPiastrelle(numPiastrelle):
   for _ in range(numPiastrelle):
-    x = random.randint(-bound, bound)
-    y = random.randint(-bound, bound)
+    x = random.randint(0, bound)
+    y = random.randint(0, bound)
     c = colors[random.randint(0, len(colors)-1)]
     i = random.randint(1, 1_000)
     print(f"C {x} {y} {c} {i}")
 
 def spegni(numPiastrelle):
   for _ in range(numPiastrelle):
-    x = random.randint(-bound, bound)
-    y = random.randint(-bound, bound)
+    x = random.randint(0, bound)
+    y = random.randint(0, bound)
     print(f"S {x} {y}")
 
 def stampaTutti():
-  for y in range(-bound, bound+1):
-    for x in range(-bound, bound+1):
+  for y in range(0, bound+1):
+    for x in range(0, bound+1):
       print(f"? {x} {y}")
 
 def generaRegole(numRegole):
@@ -26,25 +26,27 @@ def generaRegole(numRegole):
     finalColor = colors[random.randint(0, len(colors)-1)]
     count = 0
     print(f"r {finalColor}", end="")
+    availableColors = colors.copy()
     while count < maxx:
-      coeff = random.randint(0, maxx-count)
-      c = colors[random.randint(0, len(colors)-1)]
-      print(f" {coeff} {c}", end="")
+      coeff = random.randint(1, maxx-count)
+      colorIndex = random.randint(0, len(availableColors)-1)
+      print(f" {coeff} {availableColors[colorIndex]}", end="")
+      availableColors = availableColors[:colorIndex] + availableColors[colorIndex+1:]
       count += coeff
     print()
   print("s")
 
 def blocco(numBlocchi):
   for _ in range(numBlocchi):
-    x = random.randint(-bound, bound)
-    y = random.randint(-bound, bound)
+    x = random.randint(0, bound)
+    y = random.randint(0, bound)
     print(f"b {x} {y}") # blocco
     print(f"B {x} {y}") # omogeneo
 
 def propaga(numPropagazioni, forseOrdina=False):
   for i in range(numPropagazioni):
-    x = random.randint(-bound, bound)
-    y = random.randint(-bound, bound)
+    x = random.randint(0, bound)
+    y = random.randint(0, bound)
     if i % 2 == 0:
       print(f"p {x} {y}")
     else:
@@ -55,8 +57,8 @@ def propaga(numPropagazioni, forseOrdina=False):
 
 def pista(numPiste):
   for _ in range(numPiste):
-    x = random.randint(-bound, bound)
-    y = random.randint(-bound, bound)
+    x = random.randint(0, bound)
+    y = random.randint(0, bound)
     leng = random.randint(1, int(math.sqrt(bound)))
     print(f"t {x} {y}", end="")
     for _ in range(leng):
@@ -66,19 +68,19 @@ def pista(numPiste):
 
 def lung(numLung):
   for i in range(numLung):
-    x1 = random.randint(-bound, bound)
-    y1 = random.randint(-bound, bound)
+    x1 = random.randint(0, bound)
+    y1 = random.randint(0, bound)
     if i % 2 == 0:
-      x2 = random.randint(x1-100, x1+100)
-      y2 = random.randint(y1-100, y1+100)
+      x2 = random.randint(max(0, x1-100), x1+100)
+      y2 = random.randint(max(0, y1-100), y1+100)
     else:
-      x2 = random.randint(-bound, bound)
-      y2 = random.randint(-bound, bound)
+      x2 = random.randint(0, bound)
+      y2 = random.randint(0, bound)
     print(f"L {x1} {y1} {x2} {y2}")
 
-colors = ["q", "w", "e", "r"]
+colors = ["red", "yellow", "green", "blue", "black", "white", "purple"]
 dirs = ["NN", "SS", "WW", "EE", "NE", "NO", "SE", "SO"]
-bound = 500
+bound = 1000
 
 # genera piastrelle
 generaPiastrelle(500_000)
